6e1b5757b4d9db5d45e781537ce724d98da83604
OGUNSANYA TAOFEEQ <taofeeq.muhammad22@gmail.com>
2024-03-10 22:57:09 +0000

    task updated

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
+"use strict"
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    var task = true;
+    var task2 = false;
   }
 
   return [task, task2];
