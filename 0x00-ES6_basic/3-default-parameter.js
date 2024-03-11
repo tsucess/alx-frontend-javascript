e25f2c0d2dacdab8c557a6fed7e1b714b2f8e4f6
OGUNSANYA TAOFEEQ <taofeeq.muhammad22@gmail.com>
2024-03-11 06:51:11 +0000

    Task 3 updated

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,1 +1,1 @@
-export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19){ return initiialNumber + expansion1989 + expansion2019; }
+export default getSumOfHoods = (initialNumber, expansion1989 = 89, expansion2019 = 19) => initiialNumber + expansion1989 + expansion2019;
